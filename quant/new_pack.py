c153794ab5948120b5674513008bf8c4a43a09bb
Kent Liu <kent.liu@rschooltoday.com>
2024-11-13 22:15:46 -0500

    initial commit

---
+++ quant/new_pack.py
@@ -0,0 +1,43 @@
+import triton
+import triton.language as tl
+import random
+import numpy as np
+import torch
+
+def triton_quantize_and_pack_along_last_dim(data: torch.Tensor, group_size: int, bit: int):
+	assert len(data.shape) == 4
+	shape = data.shape
+	B, nh, D, T = shape
+	# ================== Get Scale & Zeros ===============
+	assert T % group_size == 0
+	num_groups = T // group_size
+	new_shape = (B * nh * D, num_groups, group_size)
+	scale_mn_shape = B, nh, D, num_groups
+	# Quantize
+	data = data.reshape(new_shape)
+	mx = torch.empty((B * nh * D, num_groups), device=data.device, dtype=data.dtype)
+	mn = torch.empty((B * nh * D, num_groups), device=data.device, dtype=data.dtype)
+	BLOCK_SIZE_N = 128
+	grid = lambda meta: (triton.cdiv(data.shape[0]*data.shape[1], BLOCK_SIZE_N),)
+	with torch.cuda.device(data.device):
+		_minmax_along_last_dim[grid](data, mn, mx,
+							 data.numel(), data.shape[0], num_groups, group_size,
+							 BLOCK_SIZE_N=BLOCK_SIZE_N, num_warps=8) 
+	# mn = torch.min(data, dim=-1, keepdim=True)[0].squeeze(-1)
+	# mx = torch.max(data, dim=-1, keepdim=True)[0].squeeze(-1)
+	scale = (mx - mn) / (2 ** bit - 1)
+	data = data - mn.unsqueeze(-1)
+	data.div_(scale.unsqueeze(-1))
+	data = data.clamp_(0, 2 ** bit - 1).round_().to(torch.int32)
+	data = data.view(-1, T)
+	feat_per_int = 32 // bit
+	packshape = (np.prod(shape[:-1]), shape[-1] // feat_per_int,)
+	code = torch.zeros(*packshape, device=data.device, dtype=torch.int32)
+	grid = lambda meta: (triton.cdiv(data.shape[0], BLOCK_SIZE_N), data.shape[1] // feat_per_int,)
+	with torch.cuda.device(data.device):
+		_pack_along_last_dim[grid](bit, data, code, data.shape[0], 
+								data.shape[1], feat_per_int, 
+								BLOCK_SIZE_N=BLOCK_SIZE_N, 
+								num_warps=8)
+	return code.view(B, nh, D, -1), scale.reshape(scale_mn_shape), mn.reshape(scale_mn_shape)
+	
